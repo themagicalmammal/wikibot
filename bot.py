e13e2849ada9654f3f05fc556a065f3772a86ea4
Dipan <dipangithub@gmail.com>
2020-11-30 22:48:09 +0530

    Code: Command
    - Removed UnSafe from /help
    - Correct exception in definition
    - Check in lang from lang_list

--- bot.py
+++ bot.py
@@ -74,7 +74,9 @@ def aid(message):
            '<b>Primary</b> \n' \
            '/definition - fetches definition of the word you want \n' \
            '/title - fetches a bunch of related titles for a word \n' \
-           '/url - gives the URL for the wiki page of the word \n\n' \
+           '/url - gives the URL for the wiki page of the word \n' \
+           '/lang - set the language you want, by typing its <a ' \
+           'href="https://meta.wikimedia.org/wiki/List_of_Wikipedias">prefix</a> \n\n' \
            '<b>Secondary</b> \n' \
            '/map - location in map with wiki database \n' \
            '/nearby - locations near a coordinate \n' \
@@ -82,9 +84,6 @@ def aid(message):
            '<b>Others</b> \n' \
            '/special - some different set of commands \n' \
            '/extra - some extra set of commands \n\n' \
-           '<b>UnSafe</b> \n' \
-           '/lang - set the language you want, wrong prefix will break commands (<a ' \
-           'href="https://meta.wikimedia.org/wiki/List_of_Wikipedias">list of languages</a>) \n\n' \
            "<b>Note:</b> If the commands don't show up type /commands\n"
     bot.send_message(chat_id=message.chat.id, text=text, parse_mode='html', reply_markup=main_keyboard())
 
@@ -183,7 +182,8 @@ def process_definition(message):
         bot.send_message(chat_id=message.chat.id, text="<b>" + def_msg + "</b> \n\n" + def_str, parse_mode='html',
                          reply_markup=main_keyboard())
     except Exception as c:
-        bot.send_message(chat_id=message.chat.id, text=c, reply_markup=main_keyboard())
+        msg = '<b>Multiple similar titles found!</b> \n\n'
+        bot.send_message(chat_id=message.chat.id, text=msg + str(c), parse_mode='html', reply_markup=main_keyboard())
 
 
 @bot.message_handler(commands=['map'])
@@ -251,20 +251,24 @@ def back(message):
 
 @bot.message_handler(commands=['lang'])
 def ln(message):
-    ln_msg = bot.reply_to(message, "Type the prefix of you <b>language</b>...", parse_mode='html')
+    ln_msg = bot.reply_to(message, "Type the prefix of your <b>language</b>...", parse_mode='html')
     bot.register_next_step_handler(ln_msg, process_ln)
 
 
 def process_ln(message):
     # noinspection PyBroadException
     try:
-        ln_msg = str(message.text)
-        ln_str = str(wikipedia.set_lang(ln_msg))
-        if ln_str == "None":
-            ln_str = "Done"
-        bot.send_message(chat_id=message.chat.id, text=ln_str, reply_markup=main_keyboard())
+        ln_msg = str(message.text).lower()
+        lang_list = ['aa', 'ab', 'abs', 'ace', 'ady', 'ady-cyrl', 'aeb', 'aeb-arab', 'aeb-latn', 'af', 'ak', 'aln', 'als', 'alt', 'am', 'ami', 'an', 'ang', 'anp', 'ar', 'arc', 'arn', 'arq', 'ary', 'arz', 'as', 'ase', 'ast', 'atj', 'av', 'avk', 'awa', 'ay', 'az', 'azb', 'ba', 'ban', 'ban-bali', 'bar', 'bat-smg', 'bbc', 'bbc-latn', 'bcc', 'bcl', 'be', 'be-tarask', 'be-x-old', 'bg', 'bgn', 'bh', 'bho', 'bi', 'bjn', 'bm', 'bn', 'bo', 'bpy', 'bqi', 'br', 'brh', 'bs', 'btm', 'bto', 'bug', 'bxr', 'ca', 'cbk-zam', 'cdo', 'ce', 'ceb', 'ch', 'cho', 'chr', 'chy', 'ckb', 'co', 'cps', 'cr', 'crh', 'crh-cyrl', 'crh-latn', 'cs', 'csb', 'cu', 'cv', 'cy', 'da', 'de', 'de-at', 'de-ch', 'de-formal', 'din', 'diq', 'dsb', 'dtp', 'dty', 'dv', 'dz', 'ee', 'egl', 'el', 'eml', 'en', 'en-ca', 'en-gb', 'eo', 'es', 'es-419', 'es-formal', 'et', 'eu', 'ext', 'fa', 'ff', 'fi', 'fit', 'fiu-vro', 'fj', 'fo', 'fr', 'frc', 'frp', 'frr', 'fur', 'fy', 'ga', 'gag', 'gan', 'gan-hans', 'gan-hant', 'gcr', 'gd', 'gl', 'glk', 'gn', 'gom', 'gom-deva', 'gom-latn', 'gor', 'got', 'grc', 'gsw', 'gu', 'gv', 'ha', 'hak', 'haw', 'he', 'hi', 'hif', 'hif-latn', 'hil', 'ho', 'hr', 'hrx', 'hsb', 'ht', 'hu', 'hu-formal', 'hy', 'hyw', 'hz', 'ia', 'id', 'ie', 'ig', 'ii', 'ik', 'ike-cans', 'ike-latn', 'ilo', 'inh', 'io', 'is', 'it', 'iu', 'ja', 'jam', 'jbo', 'jut', 'jv', 'ka', 'kaa', 'kab', 'kbd', 'kbd-cyrl', 'kbp', 'kg', 'khw', 'ki', 'kiu', 'kj', 'kjp', 'kk', 'kk-arab', 'kk-cn', 'kk-cyrl', 'kk-kz', 'kk-latn', 'kk-tr', 'kl', 'km', 'kn', 'ko', 'ko-kp', 'koi', 'kr', 'krc', 'kri', 'krj', 'krl', 'ks', 'ks-arab', 'ks-deva', 'ksh', 'ku', 'ku-arab', 'ku-latn', 'kum', 'kv', 'kw', 'ky', 'la', 'lad', 'lb', 'lbe', 'lez', 'lfn', 'lg', 'li', 'lij', 'liv', 'lki', 'lld', 'lmo', 'ln', 'lo', 'loz', 'lrc', 'lt', 'ltg', 'lus', 'luz', 'lv', 'lzh', 'lzz', 'mad', 'mai', 'map-bms', 'mdf', 'mg', 'mh', 'mhr', 'mi', 'min', 'mk', 'ml', 'mn', 'mni', 'mnw', 'mo', 'mr', 'mrh', 'mrj', 'ms', 'mt', 'mus', 'mwl', 'my', 'myv', 'mzn', 'na', 'nah', 'nan', 'nap', 'nb', 'nds', 'nds-nl', 'ne', 'new', 'ng', 'nia', 'niu', 'nl', 'nl-informal', 'nn', 'no', 'nov', 'nqo', 'nrm', 'nso', 'nv', 'ny', 'nys', 'oc', 'olo', 'om', 'or', 'os', 'pa', 'pag', 'pam', 'pap', 'pcd', 'pdc', 'pdt', 'pfl', 'pi', 'pih', 'pl', 'pms', 'pnb', 'pnt', 'prg', 'ps', 'pt', 'pt-br', 'qu', 'qug', 'rgn', 'rif', 'rm', 'rmy', 'rn', 'ro', 'roa-rup', 'roa-tara', 'ru', 'rue', 'rup', 'ruq', 'ruq-cyrl', 'ruq-latn', 'rw', 'sa', 'sah', 'sat', 'sc', 'scn', 'sco', 'sd', 'sdc', 'sdh', 'se', 'sei', 'ses', 'sg', 'sgs', 'sh', 'shi', 'shi-latn', 'shi-tfng', 'shn', 'shy-latn', 'si', 'simple', 'sk', 'skr', 'skr-arab', 'sl', 'sli', 'sm', 'sma', 'smn', 'sn', 'so', 'sq', 'sr', 'sr-ec', 'sr-el', 'srn', 'ss', 'st', 'stq', 'sty', 'su', 'sv', 'sw', 'szl', 'szy', 'ta', 'tay', 'tcy', 'te', 'tet', 'tg', 'tg-cyrl', 'tg-latn', 'th', 'ti', 'tk', 'tl', 'tly', 'tn', 'to', 'tpi', 'tr', 'tru', 'trv', 'ts', 'tt', 'tt-cyrl', 'tt-latn', 'tum', 'tw', 'ty', 'tyv', 'tzm', 'udm', 'ug', 'ug-arab', 'ug-latn', 'uk', 'ur', 'uz', 'uz-cyrl', 'uz-latn', 've', 'vec', 'vep', 'vi', 'vls', 'vmf', 'vo', 'vot', 'vro', 'wa', 'war', 'wo', 'wuu', 'xal', 'xh', 'xmf', 'xsy', 'yi', 'yo', 'yue', 'za', 'zea', 'zgh', 'zh', 'zh-classical', 'zh-cn', 'zh-hans', 'zh-hant', 'zh-hk', 'zh-min-nan', 'zh-mo', 'zh-my', 'zh-sg', 'zh-tw', 'zh-yue', 'zu']
+        if ln_msg in lang_list:
+            wikipedia.set_lang(ln_msg)
+            bot.send_message(chat_id=message.chat.id, text="Done", reply_markup=main_keyboard())
+        else:
+            wrong = 'Wrong language, please check correct <a ' \
+                    'href="https://meta.wikimedia.org/wiki/List_of_Wikipedias">prefix</a> '
+            bot.send_message(chat_id=message.chat.id, text=wrong, parse_mode='html', reply_markup=main_keyboard())
     except Exception:
-        bot.send_message(chat_id=message.chat.id, text="Error, setting language", reply_markup=definition(message))
+        bot.send_message(chat_id=message.chat.id, text="Error, setting language", reply_markup=main_keyboard(message))
 
 
 def main_extra():
